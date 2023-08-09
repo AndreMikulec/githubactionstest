fc05fc227e288ca751d546a2479e2eb32dea5dac
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2023-08-09 19:37:28 -0400

    AFTER experimenting TRY

--- msys2--setup-msys.sh
+++ msys2--setup-msys.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+## #!/usr/bin/bash
+
 set -x -v -e
 echo "File: msys2--setup-msys.sh"
 uname -a
 
 echo "MSYSTEM: ${MSYSTEM}"
+
+if [ "1" == "1" ]
+then
+  echo ONE
+fi
+
 export | grep MINGW
 export | grep MSYSTEM
 
-export
+exit
