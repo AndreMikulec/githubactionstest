0586d66d096fd9194485832aa750456efd751af8
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-16 05:20:41 -0400

    bashfile_write_to_GITHUB_ENV 14

---
+++ bashfile_write_to_GITHUB_ENV2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x -v -e
+# if line above NOT THERE then the REST below is not seen
+
+echo "from shell script - GREETING: ${GREETING}"
+echo "from shell script - GREETING_OUTPUT: ${GREETING_OUTPUT}"
