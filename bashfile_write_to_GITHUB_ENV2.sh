7d902488b515af66c3b1b3da4e2f0acfcd1f3dc8
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2026-08-18 01:20:39 -0400

    echo "from shell script - GREETING_OUTPUT: ${GREETING_OUTPUT}"  # no value returned

--- bashfile_write_to_GITHUB_ENV2.sh
+++ bashfile_write_to_GITHUB_ENV2.sh
@@ -3,4 +3,4 @@ set -x -v -e
 # if line above NOT THERE then the REST below is not seen
 
 echo "from shell script - GREETING: ${GREETING}"
-echo "from shell script - GREETING_OUTPUT: ${GREETING_OUTPUT}"
+echo "from shell script - GREETING_OUTPUT: ${GREETING_OUTPUT}"  # no value returned
